50dd1496f282ff170392667b326239d346e51894
Ivan Korablin <ivan.korablin@sensus.aero>
2023-03-31 16:27:00 +0300

    Add prepare button for gitlab

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const button = document.querySelector("button");
-const input = document.querySelector("input");
+const copyIssueButton = document.getElementById("copy-issue-id");
+const prepareMRButton = document.getElementById("prepare-mr");
+const inputIssue = document.getElementById("issue-name");
+const inputBranch = document.getElementById("target-branch-name");
+
+const CURRENT_TARGET_BRANCH = "v1.67.0";
 
 function main() {
   chrome.windows.getCurrent((w) => {
@@ -15,14 +19,16 @@ function main() {
       if (taskIdValue) {
         fillTextInput(taskIdValue);
       }
+
+      inputBranch.value = CURRENT_TARGET_BRANCH;
     });
   });
 }
 
 function fillTextInput(text) {
-  input.value = text;
-  input.select();
-  navigator?.clipboard.writeText(input.value);
+  inputIssue.value = text;
+  inputIssue.select();
+  navigator?.clipboard.writeText(inputIssue.value);
 }
 
 function parseOpenedTaskId(pathname = "") {
@@ -35,8 +41,63 @@ function parseOpenedTaskId(pathname = "") {
   return undefined;
 }
 
-button.addEventListener("click", async () => {
+function modifyMRDOM() {
+  const sourceBranch = document.querySelector(
+    ".merge-request-form > div > div > span > code"
+  ).innerText;
+
+  const title = document.querySelector(
+    "[data-qa-selector=issuable_form_title_field]"
+  ).value;
+
+  document.querySelector(
+    "[data-qa-selector=issuable_form_title_field]"
+  ).value = `[${sourceBranch}] ${title}`;
+
+  document.getElementById(
+    "merge_request_force_remove_source_branch"
+  ).checked = true;
+  document.getElementById("merge_request_squash").checked = true;
+}
+
+function prepareMR() {
+  chrome.windows.getCurrent((w) => {
+    chrome.tabs.query({ active: true, windowId: w.id }, (tabs) => {
+      var tab = tabs[0];
+      const tabLink = new URL(tab.url);
+
+      const sourceBranch = tabLink.searchParams.get(
+        "merge_request[source_branch]"
+      );
+
+      if (sourceBranch) {
+        const targetBranchName = inputBranch.value;
+        chrome.tabs
+          .update(tab.id, {
+            url: `${tab.url}&merge_request%5Btarget_branch%5D=${targetBranchName}`,
+          })
+          .then(() => {
+            chrome.tabs.onUpdated.addListener(function (tabId, info) {
+              console.log(tabId, info);
+              if (tabId === tab.id && info.status === "complete") {
+                chrome.scripting.executeScript({
+                  target: { tabId },
+                  func: modifyMRDOM,
+                });
+              }
+            });
+          });
+      }
+    });
+  });
+}
+
+copyIssueButton.addEventListener("click", async () => {
   await main();
+});
+
+prepareMRButton.addEventListener("click", async () => {
+  await prepareMR();
 });
 
 window.onload = main();
